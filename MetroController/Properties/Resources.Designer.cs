87ee730c4d9ccb2987aadedc678c1fdfb9f8f03e
Lukas Reuter <lukas.reuter.lr@gmail.com>
2014-01-19 14:54:33 +0100

    Added MainIcon as an embedded resource and created prelimenary TrayIcon.

--- MetroController/Properties/Resources.Designer.cs
+++ MetroController/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace MetroController.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon MetroController {
+            get {
+                object obj = ResourceManager.GetObject("MetroController", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
