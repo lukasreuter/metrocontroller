f67f4b81ae5e59a406f9495501419940a1e2bbd3
Lukas Reuter <lukas.reuter.lr@gmail.com>
2014-01-30 23:16:40 +0100

    Changed Battery converter to internal

--- MetroController/Converters/BatteryTypeValueToBatteryTypeString.cs
+++ MetroController/Converters/BatteryTypeValueToBatteryTypeString.cs
@@ -1,30 +1,34 @@
-﻿using System;
+﻿using MetroController.XInputWrapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-using MetroController.XInputWrapper;
-
 namespace MetroController.Converters {
- 
-    class BatteryTypeValueToBatteryTypeString : IValueConverter {
-    
+
+    internal class BatteryTypeValueToBatteryTypeString : IValueConverter {
+
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             byte v = (byte) value;
             switch ((BatteryTypes) v) {
                 case BatteryTypes.BATTERY_TYPE_ALKALINE:
                     return "alkaline";
+
                 case BatteryTypes.BATTERY_TYPE_DISCONNECTED:
                     return "disconnected";
+
                 case BatteryTypes.BATTERY_TYPE_NIMH:
                     return "NiMH";
+
                 case BatteryTypes.BATTERY_TYPE_UNKNOWN:
                     return "unknown";
+
                 case BatteryTypes.BATTERY_TYPE_WIRED:
                     return "wired";
+
                 default:
                     return String.Empty;
             }
