eb2d5716c0c09120ff4e0cf95a94382bb6731291
Lukas Reuter <lukas.reuter.lr@gmail.com>
2014-02-07 12:31:15 +0100

    Made InputMessageDispatcher static

--- MetroController/WindowsInput/InputMessageDispatcher.cs
+++ MetroController/WindowsInput/InputMessageDispatcher.cs
@@ -7,16 +7,7 @@ namespace MetroController.WindowsInput {
     /// <summary>
     /// Implements the InputMessageDispatcher by calling <see cref="Native.NativeMethods.SendInput"/>.
     /// </summary>
-    internal sealed class InputMessageDispatcher {
-
-        //
-        private static readonly InputMessageDispatcher instance = new InputMessageDispatcher();
-
-        internal static InputMessageDispatcher Instance { get { return instance; } }
-
-        private InputMessageDispatcher()
-        {
-        }
+    internal static class InputMessageDispatcher {
 
         /// <summary>
         /// Dispatches the specified list of <see cref="Input"/> messages in their specified order by issuing a single called to <see cref="Native.NativeMethods.SendInput"/>.
@@ -25,14 +16,18 @@ namespace MetroController.WindowsInput {
         /// <exception cref="ArgumentException">If the <paramref name="inputs"/> array is empty.</exception>
         /// <exception cref="ArgumentNullException">If the <paramref name="inputs"/> array is null.</exception>
         /// <exception cref="Exception">If the any of the commands in the <paramref name="inputs"/> array could not be sent successfully.</exception>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
-        internal void DispatchInput(Input[] inputs)
+        internal static void DispatchInput(Input[] inputs)
         {
             if (inputs == null) throw new ArgumentNullException("inputs");
             if (inputs.Length == 0) throw new ArgumentException(@"The input array was empty", "inputs");
             var successful = NativeMethods.SendInput((uint) inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
-            if (successful != inputs.Length)
-                throw new ArgumentException("Some simulated input commands were not sent successfully. The most common reason for this happening are the security features of Windows including User Interface Privacy Isolation (UIPI). Your application can only send commands to applications of the same or lower elevation. Similarly certain commands are restricted to Accessibility/UIAutomation applications. Refer to the project home page and the code samples for more information.");
+            if (successful != inputs.Length) {
+                throw new ArgumentException("Some simulated input commands were not sent successfully. " +
+                                            "The most common reason for this happening are the security features of Windows including User Interface Privacy Isolation (UIPI). " +
+                                            "Your application can only send commands to applications of the same or lower elevation. " +
+                                            "Similarly certain commands are restricted to Accessibility/UIAutomation applications. " +
+                                            "Refer to the project home page and the code samples for more information.");
+            }
         }
     }
 }
