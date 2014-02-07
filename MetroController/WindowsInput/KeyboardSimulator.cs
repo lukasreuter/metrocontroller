cffcd65017a2093b4d3743e5578b67d92aa1059b
Lukas Reuter <lukas.reuter.lr@gmail.com>
2014-02-07 12:35:24 +0100

    Removed from KeyboardSimulator.cs: _messageDispatcher, Mouse, ModifiersDown, ModifiersUp, KeysPress, ModifiedKeyStroke, TextEntry, Sleep(Timeouts)

--- MetroController/WindowsInput/KeyboardSimulator.cs
+++ MetroController/WindowsInput/KeyboardSimulator.cs
@@ -1,8 +1,4 @@
 ﻿using MetroController.WindowsInput.Native;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using System.Threading;
 
 namespace MetroController.WindowsInput {
@@ -11,24 +7,11 @@ namespace MetroController.WindowsInput {
     /// Implements the KeyboardSimulator interface by calling the an <see cref="InputMessageDispatcher"/> to simulate Keyboard gestures.
     /// </summary>
     internal sealed class KeyboardSimulator {
-        //
 
+        //
         private static readonly KeyboardSimulator instance = new KeyboardSimulator();
 
-        private readonly InputSimulator _inputSimulator = InputSimulator.Instance;
-
-        /// <summary>
-        /// The instance of the <see cref="InputMessageDispatcher"/> to use for dispatching <see cref="Input"/> messages.
-        /// </summary>
-        private readonly InputMessageDispatcher _messageDispatcher = InputMessageDispatcher.Instance;
-
         internal static KeyboardSimulator Instance { get { return instance; } }
-
-        /// <summary>
-        /// Gets the <see cref="MouseSimulator"/> instance for simulating Mouse input.
-        /// </summary>
-        /// <value>The <see cref="MouseSimulator"/> instance.</value>
-        internal MouseSimulator Mouse { get { return _inputSimulator.Mouse; } }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="KeyboardSimulator"/> class using an instance of a <see cref="InputMessageDispatcher"/> for dispatching <see cref="Input"/> messages.
@@ -37,34 +20,13 @@ namespace MetroController.WindowsInput {
         {
         }
 
-        private void ModifiersDown(InputBuilder builder, IEnumerable<VirtualKeyCode> modifierKeyCodes)
-        {
-            if (modifierKeyCodes == null) return;
-            foreach (var key in modifierKeyCodes) builder.AddKeyDown(key);
-        }
-
-        private void ModifiersUp(InputBuilder builder, IEnumerable<VirtualKeyCode> modifierKeyCodes)
-        {
-            if (modifierKeyCodes == null) return;
-
-            // Key up in reverse (I miss LINQ)
-            var stack = new Stack<VirtualKeyCode>(modifierKeyCodes);
-            while (stack.Count > 0) builder.AddKeyUp(stack.Pop());
-        }
-
-        private void KeysPress(InputBuilder builder, IEnumerable<VirtualKeyCode> keyCodes)
-        {
-            if (keyCodes == null) return;
-            foreach (var key in keyCodes) builder.AddKeyPress(key);
-        }
-
         /// <summary>
         /// Sends the list of <see cref="Input"/> messages using the <see cref="InputMessageDispatcher"/> instance.
         /// </summary>
         /// <param name="inputList">The <see cref="System.Array"/> of <see cref="Input"/> messages to send.</param>
-        private void SendSimulatedInput(Input[] inputList)
+        private static void SendSimulatedInput(Input[] inputList)
         {
-            _messageDispatcher.DispatchInput(inputList);
+            InputMessageDispatcher.DispatchInput(inputList);
         }
 
         /// <summary>
@@ -101,111 +63,12 @@ namespace MetroController.WindowsInput {
         }
 
         /// <summary>
-        /// Simulates a key press for each of the specified key codes in the order they are specified.
-        /// </summary>
-        /// <param name="keyCodes"></param>
-        internal KeyboardSimulator KeyPress(params VirtualKeyCode[] keyCodes)
-        {
-            var builder = new InputBuilder();
-            KeysPress(builder, keyCodes);
-            SendSimulatedInput(builder.ToArray());
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a simple modified keystroke like CTRL-C where CTRL is the modifierKey and C is the key.
-        /// The flow is Modifier KeyDown, Key Press, Modifier KeyUp.
-        /// </summary>
-        /// <param name="modifierKeyCode">The modifier key</param>
-        /// <param name="keyCode">The key to simulate</param>
-        internal KeyboardSimulator ModifiedKeyStroke(VirtualKeyCode modifierKeyCode, VirtualKeyCode keyCode)
-        {
-            ModifiedKeyStroke(new[] { modifierKeyCode }, new[] { keyCode });
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a modified keystroke where there are multiple modifiers and one key like CTRL-ALT-C where CTRL and ALT are the modifierKeys and C is the key.
-        /// The flow is Modifiers KeyDown in order, Key Press, Modifiers KeyUp in reverse order.
-        /// </summary>
-        /// <param name="modifierKeyCodes">The list of modifier keys</param>
-        /// <param name="keyCode">The key to simulate</param>
-        internal KeyboardSimulator ModifiedKeyStroke(IEnumerable<VirtualKeyCode> modifierKeyCodes, VirtualKeyCode keyCode)
-        {
-            ModifiedKeyStroke(modifierKeyCodes, new[] { keyCode });
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a modified keystroke where there is one modifier and multiple keys like CTRL-K-C where CTRL is the modifierKey and K and C are the keys.
-        /// The flow is Modifier KeyDown, Keys Press in order, Modifier KeyUp.
-        /// </summary>
-        /// <param name="modifierKey">The modifier key</param>
-        /// <param name="keyCodes">The list of keys to simulate</param>
-        internal KeyboardSimulator ModifiedKeyStroke(VirtualKeyCode modifierKey, IEnumerable<VirtualKeyCode> keyCodes)
-        {
-            ModifiedKeyStroke(new[] { modifierKey }, keyCodes);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a modified keystroke where there are multiple modifiers and multiple keys like CTRL-ALT-K-C where CTRL and ALT are the modifierKeys and K and C are the keys.
-        /// The flow is Modifiers KeyDown in order, Keys Press in order, Modifiers KeyUp in reverse order.
-        /// </summary>
-        /// <param name="modifierKeyCodes">The list of modifier keys</param>
-        /// <param name="keyCodes">The list of keys to simulate</param>
-        internal KeyboardSimulator ModifiedKeyStroke(IEnumerable<VirtualKeyCode> modifierKeyCodes, IEnumerable<VirtualKeyCode> keyCodes)
-        {
-            var builder = new InputBuilder();
-            var virtualKeyCodes = modifierKeyCodes as VirtualKeyCode[] ?? modifierKeyCodes.ToArray();
-            ModifiersDown(builder, virtualKeyCodes);
-            KeysPress(builder, keyCodes);
-            ModifiersUp(builder, virtualKeyCodes);
-
-            SendSimulatedInput(builder.ToArray());
-            return this;
-        }
-
-        /// <summary>
-        /// Calls the Win32 SendInput method with a stream of KeyDown and KeyUp messages in order to simulate uninterrupted text entry via the keyboard.
-        /// </summary>
-        /// <param name="text">The text to be simulated.</param>
-        internal KeyboardSimulator TextEntry(string text)
-        {
-            if (text.Length > uint.MaxValue / 2) throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, "The text parameter is too long. It must be less than {0} characters.", uint.MaxValue / 2), "text");
-            var inputList = new InputBuilder().AddCharacters(text).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a single character text entry via the keyboard.
-        /// </summary>
-        /// <param name="character">The unicode character to be simulated.</param>
-        internal KeyboardSimulator TextEntry(char character)
-        {
-            var inputList = new InputBuilder().AddCharacter(character).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
         /// Sleeps the executing thread to create a pause between simulated inputs.
         /// </summary>
         /// <param name="millsecondsTimeout">The number of milliseconds to wait.</param>
         internal KeyboardSimulator Sleep(int millsecondsTimeout)
         {
             Thread.Sleep(millsecondsTimeout);
-            return this;
-        }
-
-        /// <summary>
-        /// Sleeps the executing thread to create a pause between simulated inputs.
-        /// </summary>
-        /// <param name="timeout">The time to wait.</param>
-        internal KeyboardSimulator Sleep(TimeSpan timeout)
-        {
-            Thread.Sleep(timeout);
             return this;
         }
     }
