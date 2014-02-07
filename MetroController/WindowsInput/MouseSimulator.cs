7700119926e51c52bd430dcefe6dc95d9ea529e5
Lukas Reuter <lukas.reuter.lr@gmail.com>
2014-02-07 12:30:51 +0100

    Removed from MouseSimulator.cs: Keyboard, _messageDispatcher, MoveMouseTo, MoveMouseToPositionOnVirtualdesktop, LeftButtonClick, LeftButtonDoubleClick, RightButtonClick, RightButtonDoubleClick, XButtonDown, XButtonUp, XButtonClick, XButtonDoubleClick, HorizontalScroll, Sleep(Timeouts). Removed return value from: MoveMouseBy, LeftButtonDown, LeftButtonUp, RightButtonDown, RightButtonUp, VerticalScroll, Sleep(Milliseconds). Made the whole class static.

--- MetroController/WindowsInput/MouseSimulator.cs
+++ MetroController/WindowsInput/MouseSimulator.cs
@@ -1,5 +1,4 @@
 ﻿using MetroController.WindowsInput.Native;
-using System;
 using System.Threading;
 
 namespace MetroController.WindowsInput {
@@ -7,39 +6,18 @@ namespace MetroController.WindowsInput {
     /// <summary>
     /// Implements the MouseSimulator interface by calling the <see cref="InputMessageDispatcher"/> to simulate Mouse gestures.
     /// </summary>
-    internal sealed class MouseSimulator {
+    internal static class MouseSimulator {
 
         //
         private const int MOUSE_WHEEL_CLICK_SIZE = 120;
-
-        private readonly InputSimulator _inputSimulator = InputSimulator.Instance;
-
-        /// <summary>
-        /// The instance of the <see cref="InputMessageDispatcher"/> to use for dispatching <see cref="Input"/> messages.
-        /// </summary>
-        private readonly InputMessageDispatcher _messageDispatcher = InputMessageDispatcher.Instance;
-
-        private static readonly MouseSimulator instance = new MouseSimulator();
-
-        internal static MouseSimulator Instance { get { return instance; } }
-
-        /// <summary>
-        /// Gets the <see cref="KeyboardSimulator"/> instance for simulating Keyboard input.
-        /// </summary>
-        /// <value>The <see cref="KeyboardSimulator"/> instance.</value>
-        internal KeyboardSimulator Keyboard { get { return _inputSimulator.Keyboard; } }
-
-        private MouseSimulator()
-        {
-        }
 
         /// <summary>
         /// Sends the list of <see cref="Input"/> messages using the <see cref="InputMessageDispatcher"/> instance.
         /// </summary>
         /// <param name="inputList">The <see cref="System.Array"/> of <see cref="Input"/> messages to send.</param>
-        private void SendSimulatedInput(Input[] inputList)
+        private static void SendSimulatedInput(Input[] inputList)
         {
-            _messageDispatcher.DispatchInput(inputList);
+            InputMessageDispatcher.DispatchInput(inputList);
         }
 
         /// <summary>
@@ -47,201 +25,65 @@ namespace MetroController.WindowsInput {
         /// </summary>
         /// <param name="pixelDeltaX">The distance in pixels to move the mouse horizontally.</param>
         /// <param name="pixelDeltaY">The distance in pixels to move the mouse vertically.</param>
-        internal MouseSimulator MoveMouseBy(int pixelDeltaX, int pixelDeltaY)
+        internal static void MoveMouseBy(int pixelDeltaX, int pixelDeltaY)
         {
             var inputList = new InputBuilder().AddRelativeMouseMovement(pixelDeltaX, pixelDeltaY).ToArray();
             SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates mouse movement to the specified location on the primary display device.
-        /// </summary>
-        /// <param name="absoluteX">The destination's absolute X-coordinate on the primary display device where 0 is the extreme left hand side of the display device and 65535 is the extreme right hand side of the display device.</param>
-        /// <param name="absoluteY">The destination's absolute Y-coordinate on the primary display device where 0 is the top of the display device and 65535 is the bottom of the display device.</param>
-        internal MouseSimulator MoveMouseTo(double absoluteX, double absoluteY)
-        {
-            var inputList = new InputBuilder().AddAbsoluteMouseMovement((int) Math.Truncate(absoluteX), (int) Math.Truncate(absoluteY)).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates mouse movement to the specified location on the Virtual Desktop which includes all active displays.
-        /// </summary>
-        /// <param name="absoluteX">The destination's absolute X-coordinate on the virtual desktop where 0 is the left hand side of the virtual desktop and 65535 is the extreme right hand side of the virtual desktop.</param>
-        /// <param name="absoluteY">The destination's absolute Y-coordinate on the virtual desktop where 0 is the top of the virtual desktop and 65535 is the bottom of the virtual desktop.</param>
-        internal MouseSimulator MoveMouseToPositionOnVirtualDesktop(double absoluteX, double absoluteY)
-        {
-            var inputList = new InputBuilder().AddAbsoluteMouseMovementOnVirtualDesktop((int) Math.Truncate(absoluteX), (int) Math.Truncate(absoluteY)).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
         }
 
         /// <summary>
         /// Simulates a mouse left button down gesture.
         /// </summary>
-        internal MouseSimulator LeftButtonDown()
+        internal static void LeftButtonDown()
         {
             var inputList = new InputBuilder().AddMouseButtonDown(MouseButton.LeftButton).ToArray();
             SendSimulatedInput(inputList);
-            return this;
         }
 
         /// <summary>
         /// Simulates a mouse left button up gesture.
         /// </summary>
-        internal MouseSimulator LeftButtonUp()
+        internal static void LeftButtonUp()
         {
             var inputList = new InputBuilder().AddMouseButtonUp(MouseButton.LeftButton).ToArray();
             SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a mouse left-click gesture.
-        /// </summary>
-        internal MouseSimulator LeftButtonClick()
-        {
-            var inputList = new InputBuilder().AddMouseButtonClick(MouseButton.LeftButton).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a mouse left button double-click gesture.
-        /// </summary>
-        internal MouseSimulator LeftButtonDoubleClick()
-        {
-            var inputList = new InputBuilder().AddMouseButtonDoubleClick(MouseButton.LeftButton).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
         }
 
         /// <summary>
         /// Simulates a mouse right button down gesture.
         /// </summary>
-        internal MouseSimulator RightButtonDown()
+        internal static void RightButtonDown()
         {
             var inputList = new InputBuilder().AddMouseButtonDown(MouseButton.RightButton).ToArray();
             SendSimulatedInput(inputList);
-            return this;
         }
 
         /// <summary>
         /// Simulates a mouse right button up gesture.
         /// </summary>
-        internal MouseSimulator RightButtonUp()
+        internal static void RightButtonUp()
         {
             var inputList = new InputBuilder().AddMouseButtonUp(MouseButton.RightButton).ToArray();
             SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a mouse right button click gesture.
-        /// </summary>
-        internal MouseSimulator RightButtonClick()
-        {
-            var inputList = new InputBuilder().AddMouseButtonClick(MouseButton.RightButton).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a mouse right button double-click gesture.
-        /// </summary>
-        internal MouseSimulator RightButtonDoubleClick()
-        {
-            var inputList = new InputBuilder().AddMouseButtonDoubleClick(MouseButton.RightButton).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a mouse X button down gesture.
-        /// </summary>
-        /// <param name="buttonId">The button id.</param>
-        internal MouseSimulator XButtonDown(int buttonId)
-        {
-            var inputList = new InputBuilder().AddMouseXButtonDown(buttonId).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a mouse X button up gesture.
-        /// </summary>
-        /// <param name="buttonId">The button id.</param>
-        internal MouseSimulator XButtonUp(int buttonId)
-        {
-            var inputList = new InputBuilder().AddMouseXButtonUp(buttonId).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a mouse X button click gesture.
-        /// </summary>
-        /// <param name="buttonId">The button id.</param>
-        internal MouseSimulator XButtonClick(int buttonId)
-        {
-            var inputList = new InputBuilder().AddMouseXButtonClick(buttonId).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a mouse X button double-click gesture.
-        /// </summary>
-        /// <param name="buttonId">The button id.</param>
-        internal MouseSimulator XButtonDoubleClick(int buttonId)
-        {
-            var inputList = new InputBuilder().AddMouseXButtonDoubleClick(buttonId).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
         }
 
         /// <summary>
         /// Simulates mouse vertical wheel scroll gesture.
         /// </summary>
         /// <param name="scrollAmountInClicks">The amount to scroll in clicks. A positive value indicates that the wheel was rotated forward, away from the user; a negative value indicates that the wheel was rotated backward, toward the user.</param>
-        internal MouseSimulator VerticalScroll(int scrollAmountInClicks)
+        internal static void VerticalScroll(int scrollAmountInClicks)
         {
             var inputList = new InputBuilder().AddMouseVerticalWheelScroll(scrollAmountInClicks * MOUSE_WHEEL_CLICK_SIZE).ToArray();
             SendSimulatedInput(inputList);
-            return this;
-        }
-
-        /// <summary>
-        /// Simulates a mouse horizontal wheel scroll gesture. Supported by Windows Vista and later.
-        /// </summary>
-        /// <param name="scrollAmountInClicks">The amount to scroll in clicks. A positive value indicates that the wheel was rotated to the right; a negative value indicates that the wheel was rotated to the left.</param>
-        internal MouseSimulator HorizontalScroll(int scrollAmountInClicks)
-        {
-            var inputList = new InputBuilder().AddMouseHorizontalWheelScroll(scrollAmountInClicks * MOUSE_WHEEL_CLICK_SIZE).ToArray();
-            SendSimulatedInput(inputList);
-            return this;
         }
 
         /// <summary>
         /// Sleeps the executing thread to create a pause between simulated inputs.
         /// </summary>
         /// <param name="millsecondsTimeout">The number of milliseconds to wait.</param>
-        internal MouseSimulator Sleep(int millsecondsTimeout)
+        internal static void Sleep(int millsecondsTimeout)
         {
             Thread.Sleep(millsecondsTimeout);
-            return this;
-        }
-
-        /// <summary>
-        /// Sleeps the executing thread to create a pause between simulated inputs.
-        /// </summary>
-        /// <param name="timeout">The time to wait.</param>
-        internal MouseSimulator Sleep(TimeSpan timeout)
-        {
-            Thread.Sleep(timeout);
-            return this;
         }
     }
 }
